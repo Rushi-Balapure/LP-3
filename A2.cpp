3f0406ef1c898c51615eb8774dc4f5a077c42317
Rushi Balapure <97079398+Rushi-Balapure@users.noreply.github.com>
2024-11-05 08:54:18 +0530

    Update A2.cpp

--- A2.cpp
+++ A2.cpp
@@ -1,58 +1,76 @@
-#include<iostream>
-#include<vector>
 #include<bits/stdc++.h>
 using namespace std;
-class Node{
+
+class Node {
 public:
     int val;
     string symbol;
     string huffman;
-    Node* left;
-    Node* right;
-    Node(int val, string symbol)
-    {
-        this->val=val;
-        this->symbol=symbol;
-        this->left=NULL;
-        this->right=NULL;
-        this->huffman="";
+    Node *left;
+    Node *right;
+
+    Node(int n, string s) {
+        this -> val = n;
+        this -> symbol = s;
+        this -> huffman = "";
+        this -> left = nullptr;
+        this -> right = nullptr;
     }
 };
-struct Compare{
-    bool operator()(const Node* left, const Node* right)
-    {
-        return left->val > right->val;
+
+
+struct Compare {
+    bool operator()(const Node *a, const Node *b) {
+        return a -> val > b -> val;
     }
 };
-class Huffman
-{
-    Node* root;
-    priority_queue<Node*,vector<Node*>,Compare> pq;
-    void printCodes(Node* node,const string& val="")
-    {
-        string newVal=val+node->huffman;
-        if(node->left!=NULL)printCodes(node->left,newVal);
-        if(node->right!=NULL)printCodes(node->right,newVal);
-        if(node->left==NULL && node->right==NULL)cout<<node->symbol<<"->"<<newVal<<endl;
+
+
+class HuffmanEncoding {
+    Node *root;
+    string inputText;
+    unordered_map<string, int> freqs;
+    unordered_map<string, string> codes;
+    priority_queue<Node *, vector<Node *>, Compare> pq;
+
+    void encode(Node *node, const string &val = "") {
+        string newVal = val + node -> huffman;
+
+        if(node -> left != nullptr) {
+            encode(node -> left, newVal);
+        }
+        if(node -> right != nullptr) {
+            encode(node -> right, newVal);
+        }
+
+        if(node -> left == nullptr && node -> right == nullptr) {
+            codes[node -> symbol] = newVal;
+        }
     }
-    public:
-    Huffman(vector<string> &arr,vector<int> &freq)
-    {
-        this->root=NULL;
-        int n=arr.size();
-        for(int i=0;i<n;i++)
-        {
-            Node *node=new Node(freq[i],arr[i]);
+
+public:
+    HuffmanEncoding(string text) {
+        this -> inputText = text;
+        this -> root = nullptr;
+
+        for(char c : inputText) {
+            string s(1, c);
+            freqs[s]++;
+        }
+
+        for(auto it : freqs) {
+            Node *node = new Node(it.second, it.first);
             pq.push(node);
         }
     }
-    void buildTree()
-    {
+
+    void buildTree() {
         while(pq.size() > 1) {
             Node *l = pq.top();
             pq.pop();
             Node *r = pq.top();
             pq.pop();
+
             l -> huffman = "0";
             r -> huffman = "1";
 
@@ -66,19 +84,34 @@ class Huffman
         pq.pop();
     }
 
-    void display() {
-        if(this -> root != nullptr) {
-            printCodes(this -> root);
+    void displayResult() {
+        cout<<"The character coding is as follows: "<<endl;
+        encode(root);
+        for(auto it : codes) {
+            cout<<it.first<<" --> "<<it.second<<endl;
         }
+        cout<<endl;
+
+        string encodedText = "";
+        for(char c : inputText) {
+            string s(1, c);
+            encodedText += codes[s];
+        }
+
+        cout<<"The encoded text is.."<<endl;
+        cout<<encodedText<<endl;
     }
 };
-int main()
-{
-    vector<string> stringArr = {"a", "b", "c", "d", "e"};
-    vector<int> freqArr = {100, 12, 24, 105, 38};
 
-    Huffman huffmanEncoding(stringArr, freqArr);
-    huffmanEncoding.buildTree();
-    huffmanEncoding.display();
 
+int main() {
+    string text;
+    cout<<"Enter text to encode"<<endl;
+    cin>>text;
+
+    HuffmanEncoding he(text);
+    he.buildTree();
+    he.displayResult();
+
+    return 0;
 }
